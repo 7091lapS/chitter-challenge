f7a9d1180b8093c6a5f73e967927df1041aa0f45
Antonio Voce <antonio.v0c3@gmail.com>
2015-08-28 00:54:23 +0100

    time month and year display on with each peep

--- Gemfile
+++ Gemfile
@@ -12,10 +12,10 @@ group :test do
   gem 'byebug'
   gem 'factory_girl', "~> 4.0"
   gem 'rspec'
+  gem 'timecop'
   gem 'capybara'
   gem 'rubocop-rspec'
   gem 'rubocop'
   gem 'database_cleaner'
   gem 'coveralls', require: false
-  gem 'database_cleaner'
 end
--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -2,6 +2,10 @@ feature 'Viewing peeps' do
 
   let!(:user) { FactoryGirl.create(:user) }
 
+  # before do
+  #   Timecop.freeze(Time.local(1990))
+  # end
+
   scenario 'I can see existing peeps on the peeps page' do
     user.peeps.create(content: 'hello')
     visit '/peeps'
@@ -16,7 +20,7 @@ feature 'Viewing peeps' do
     visit '/peeps'
 
     within 'ul#peeps' do
-      expect(page).to have_content('JD03: test peep - date still to let')
+      expect(page).to have_content("JD03: test peep · #{Time.now.strftime("%d %b '%y - %H:%M")}")
     end
   end
 end
